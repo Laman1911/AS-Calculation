84b15b2d8db1be8349607d2fa487ea2d8166de4e
LamanH19 <lahu0001@stud.kea.dk>
2025-12-15 23:40:21 +0100

    add project files

--- src/main/java/dk/eak/kalkulation/controller/ProjektController.java
+++ src/main/java/dk/eak/kalkulation/controller/ProjektController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@Profile("dev")
+
 @RequestMapping("/projekter")
 public class ProjektController {
     private final ProjektService service;
